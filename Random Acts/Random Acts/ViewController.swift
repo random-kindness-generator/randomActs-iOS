a6eb9ac51f75bc55ace940029c8b3fe6260dfcf1
Sergey Osipyan <osipyansergey@gmail.com>
2019-05-28 13:42:32 -0700

    create new Xcode projet

---
+++ Random Acts/Random Acts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random Acts
+//
+//  Created by Sergey Osipyan on 5/28/19.
+//  Copyright © 2019 Random Acts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
