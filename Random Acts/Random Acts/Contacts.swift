420a6904e1db3532a82b22c033bb0b3fbe84f08b
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MacBook-Pro.local>
2019-05-28 17:24:05 -0400

    created model

---
+++ Random Acts/Random Acts/Contacts.swift
@@ -0,0 +1,14 @@
+//
+//  Contacts.swift
+//  Random Acts
+//
+//  Created by Diante Lewis-Jolley on 5/28/19.
+//  Copyright © 2019 Random Acts. All rights reserved.
+//
+
+import Foundation
+
+
+struct Contacts: Codable {
+    
+}
