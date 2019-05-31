478ca091c8c05e971dc8ad43a462c8b97f704fe7
Sergey Osipyan <osipyansergey@gmail.com>
2019-05-31 12:45:00 -0700

    Networking requests all done works perfect

--- Random Acts/Random Acts/ActsOfServiceTableViewController.swift
+++ Random Acts/Random Acts/ActsOfServiceTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ActsOfServiceTableViewController: UITableViewController {
 
     
-    
+    var actions: [Activity] = []
     
     @IBAction func addNewGestureButton(_ sender: Any) {
         
@@ -24,12 +24,20 @@ class ActsOfServiceTableViewController: UITableViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ActionController.shared.fetchActions { (actions) in
+            self.actions = actions
+            self.tableView.reloadData()
+        }
+    }
+    
     // MARK: - Table view data source
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return actions.count
     }
     
     
@@ -37,6 +45,7 @@ class ActsOfServiceTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "gestureCell", for: indexPath)
         
         // Configure the cell...
+        cell.textLabel?.text = actions[indexPath.row].action
         style(cell: cell)
         
         return cell
@@ -53,25 +62,30 @@ class ActsOfServiceTableViewController: UITableViewController {
 
 
     }
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let action = actions[indexPath.row]
+            ActionController.shared.deleteAction(with: action) { (bool) in
+                
+            }
+            actions.remove(at: indexPath.row)
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            
+        }
+    }
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "editGesture" {
+            let destination = segue.destination as! GestureDetailViewController
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            destination.action = self.actions[indexPath.row]
+        }
+    }
     
 }
